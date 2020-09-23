d72d22cfdd2a437f959ab71b9028626cf4c13d7a
Mark Xie <ly.markxie@gmail.com>
2020-09-23 14:12:27 -0400

    First Commit

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorldProject {
+	class Program {
+		public static void Main() {
+			Console.WriteLine("Hello World!");
+
+			int v = Factorial.Factor(5);
+		}
+	}
+	class Factorial {
+		public int Factor(int factor) {
+			if ((factor != 1) && (factor > 0)) {
+				return (factor * Factor(factor - 1));
+			}
+			else {
+				return 1;
+			}
+			
+		}
+	}
+}
---
+++ MyFirstCSharp/CSharpBasic1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasic1 {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+
+		}
+	}
+	static int factor(int[] args) {
+	}
+}
