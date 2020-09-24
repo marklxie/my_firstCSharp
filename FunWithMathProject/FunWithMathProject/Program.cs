8aa2f5aaea85b2b90e2a2b63933fa2b6d269e6e3
Mark Xie <ly.markxie@gmail.com>
2020-09-24 11:07:25 -0400

    Even Odd Project Added

--- FunWithMathProject/FunWithMathProject/Program.cs
+++ FunWithMathProject/FunWithMathProject/Program.cs
@@ -3,19 +3,22 @@
 namespace FunWithMathProject {
 	class Program {
 		static void Main(string[] args) {
-			int[] numbers = { 573, 595, 468, 191, 964 };
+			/*int[] numbers = { 573, 595, 468, 191, 964 };
 			int hold = 0;
 			for (int i = 0; i < 5; i++) {
-				hold = hold + numbers[i];
+				hold += numbers[i];
 			}
 
-			int avg;
-			int remainder;
-			avg = hold / 5;
-			remainder = hold / avg;
+			var avg = hold / 5.0;
+			int intAvg = hold / 5;
+			var remainder = hold % intAvg;
 			Console.WriteLine(hold);
 			Console.WriteLine(avg);
-			Console.WriteLine(remainder);
+			Console.WriteLine(intAvg);
+			Console.WriteLine(remainder);*/
+
+			int num;
+			
 
 		}
 	}
---
+++ OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OddOrEven {
+	class Program {
+		static void Main(string[] args) {
+			
+			Console.Write("Enter an integer: ");
+			var response = Console.ReadLine();
+			//Console.Write(response);
+			var nbr = Convert.ToInt32(response);
+
+			if ((nbr % 2) == 0) {
+				Console.WriteLine($"The number {response} is even.");
+			}
+			else {
+				Console.WriteLine($"The number {response} is odd.");
+			}
+		}
+	}
+}
