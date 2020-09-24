48e6f8b8449138c178aed1f9e501454d2698d4f9
Mark Xie <ly.markxie@gmail.com>
2020-09-24 16:28:07 -0400

    Foreach practice

--- ForExample/ForExample/Program.cs
+++ ForExample/ForExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Globalization;
 
 namespace ForExample {
@@ -18,6 +19,42 @@ namespace ForExample {
 			}
 
 			Console.WriteLine($"The answer is {total}.");
+
+			int[] ints = { 17, 94, 51, 51, 89, 47, 68, 40, 65, 41, 41, 63, 39, 61, 44 };
+			var sumOfOdds = 0;
+
+			for(int i = 0; i < 15; i++) {
+				if(ints[i] % 2 == 1) {
+					sumOfOdds += ints[i];
+					Console.WriteLine(ints[i]);
+				}
+			}
+
+
+			Console.WriteLine($"The answer is {sumOfOdds}.");
+
+			for(int i = 0; i < 15; i++) {
+				if(ints[i] % 2 == 1) {
+					sumOfOdds += ints[i];
+					Console.WriteLine(ints[i]);
+				}
+			}
+			Console.WriteLine($"The answer is {sumOfOdds}.");
+
+			int[] list = { 28, 62, 89, 67, 6, 62, 88, 43, 10, 26};
+			total = 0;
+			foreach(int num in list) {
+				total += num;
+			}
+			Console.WriteLine($"\nThe answer is {total}.");
+
+			string[] names = { "Greg", "Elena", "Manami", "Christian", "Tracy", "Mark", "Yvonne" };
+			foreach(string name in names) {
+				foreach(int num in list) {
+					if(num >= 50)
+						Console.WriteLine($"{num}{name}");
+				}
+			}
 		}
 	}
 }
--- FunWithMathProject/FunWithMathProject/Program.cs
+++ FunWithMathProject/FunWithMathProject/Program.cs
@@ -3,7 +3,7 @@
 namespace FunWithMathProject {
 	class Program {
 		static void Main(string[] args) {
-			/*int[] numbers = { 573, 595, 468, 191, 964 };
+			int[] numbers = { 573, 595, 468, 191, 964 };
 			int hold = 0;
 			for (int i = 0; i < 5; i++) {
 				hold += numbers[i];
@@ -15,9 +15,7 @@ namespace FunWithMathProject {
 			Console.WriteLine(hold);
 			Console.WriteLine(avg);
 			Console.WriteLine(intAvg);
-			Console.WriteLine(remainder);*/
-
-			int num;
+			Console.WriteLine(remainder);
 			
 
 		}
