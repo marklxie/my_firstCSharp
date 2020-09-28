48377ba67898dd44abf0f2cd64af98e2828b804f
Mark Xie <ly.markxie@gmail.com>
2020-09-28 09:53:05 -0400

    New Project for short statements

---
+++ OtherStatementsPractice/OtherStatementsPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OtherStatementsPractice {
+	class Program {
+		static void Main(string[] args) {
+			
+			//Ternary Operator
+			bool isnotBlack = true;
+			string color;
+
+			if(isnotBlack == true) {
+				color = "There is color";
+			}
+			else {
+				color = "It's Black";
+			}
+
+			Console.WriteLine(color);
+
+			var todayisMonday = false;
+			var dayofWeek = "";
+
+			dayofWeek = (todayisMonday == true) ? "Monday" : "Not Monday";
+
+			Console.WriteLine(dayofWeek);
+			
+		}
+	}
+}
--- PigDice/PigDice/Program.cs
+++ PigDice/PigDice/Program.cs
@@ -3,7 +3,7 @@
 namespace PigDice {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }
