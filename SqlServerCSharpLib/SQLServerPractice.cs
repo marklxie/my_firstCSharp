79b41c9128db243e9a087105c75927ce9d77230f
Mark Xie <ly.markxie@gmail.com>
2020-10-07 10:54:29 -0400

    Practicing the SQL stuff

---
+++ SqlServerCSharpLib/SQLServerPractice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SqlServerCSharpLib {
+
+	class SQLServerPractice {
+		public SqlConnection Connection = null;
+		public bool Connect(string server, string instance, string database) {
+			string serverConnect = $"server={server}\\{instance};database={database};trusted_connection=true";
+			Connection = new SqlConnection(serverConnect);
+			Connection.Open();
+			if(Connection.State != System.Data.ConnectionState.Open) {
+				throw new Exception("The connection didn't work");
+			}
+			return true;
+		}
+		public void SqlCommand(string sqlQuerry) {
+			var command = new SqlCommand(sqlQuerry, Connection);
+			var reader = command.ExecuteReader();
+			Student stud = new Student();
+			while(reader.Read()) {
+				stud.Id = Convert.ToInt32(reader["ID"]);
+				stud.Firstname = Convert.ToString(reader["Firstname"]);
+				stud.Lastname = Convert.ToString(reader["Lastname"]);
+				stud.MajorId = reader.IsDBNull("MajorId") ?(int?) null : Convert.ToInt32(reader["MajorId"]); 
+
+			}
+		}
+	}
+}
